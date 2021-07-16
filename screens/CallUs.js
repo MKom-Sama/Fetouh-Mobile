8b80bd2430da3e60f9c6687027f774e1436d0815
MKom <mkomsany3@yahoo.com>
2021-07-16 19:37:16 +0200

    map and callus functionality no UI

--- screens/CallUs.js
+++ screens/CallUs.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+import { Button } from 'react-native-paper'
+
 import AppTitle from '../components/visuals/AppTitle'
+
+import { callNumber,openGPS } from '../utils/linking'
 
 export default function CallUs() {
     return (
         <View>
             <AppTitle />
+            <Button mode="contained" onPress={() => {
+                callNumber('01015047515')
+            }}>
+                call us
+            </Button>
+            <Button mode="contained" onPress={()=>openGPS(31.50715753006112, 31.826428123336655)}>
+                location
+            </Button>
         </View>
     )
 }
---
+++ utils/linking.js
@@ -0,0 +1,41 @@
+import { Linking, Alert, Platform } from 'react-native';
+
+
+const callNumber = phone => {
+  console.log('callNumber ----> ', phone);
+  let phoneNumber = phone;
+  if (Platform.OS !== 'android') {
+    phoneNumber = `telprompt:${phone}`;
+  }
+  else  {
+    phoneNumber = `tel:${phone}`;
+  }
+  Linking.canOpenURL(phoneNumber)
+  .then(supported => {
+    if (!supported) {
+      Alert.alert('Phone number is not available');
+    } else {
+      return Linking.openURL(phoneNumber);
+    }
+  })
+  .catch(err => console.log(err));
+};
+
+const openGPS = (lat, lng) => {
+  var scheme = Platform.OS === 'ios' ? 'maps:' : 'geo:';
+  // var url = scheme + `${lat},${lng}`;
+
+  let androidLabel = 'مطعم وكافيه فتوح اخوان فتوح مطعم وكافيه';
+  let iosLabel = 'شارع 65 20L'
+  const url = Platform.select({
+    ios: "maps:" + lat + "," + lng + "?q=" + iosLabel,
+    android: "geo:" + lat + "," + lng + "?q=" + androidLabel
+  });
+  // `google.navigation:${lat+lng}=100+101`
+  Linking.openURL(url);
+}
+
+export {
+  callNumber,
+  openGPS
+}
