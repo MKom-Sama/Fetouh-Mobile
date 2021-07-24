53baae030763bad3f4ced9d685315359f5f09ae5
MKom <mkomsany3@yahoo.com>
2021-07-24 09:04:01 +0200

    styled AppTitle border radius

--- components/visuals/AppTitle.js
+++ components/visuals/AppTitle.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function AppTitle() {
-    return (
-        <View style={{borderBottomLeftRadius:50,borderBottomRightRadius:50,backgroundColor:'#d50000'}}>
-            <Text style={styles.title}>Fetouh 63</Text>
-        </View>
-    )
+  return (
+    <View
+      style={{
+        borderBottomLeftRadius: 40,
+        borderBottomRightRadius: 40,
+        backgroundColor: "#d50000",
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+      }}
+    >
+      <Text style={styles.title}>Fetouh 63 </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    title: {
-        textAlign: 'center',
-        fontFamily: 'pacifico',
-        fontSize: 35,
-        color: '#FFFFFF',
-    }
-})
+  title: {
+    textAlign: "center",
+    fontFamily: "pacifico",
+    fontSize: 35,
+    color: "#FFFFFF",
+  },
+});
